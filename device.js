276f839ed91fddbd0803932a3c0ae169499432a6
Pmooney614 <pmooney614@hotmail.com>
2025-11-26 13:13:49 +1300

    Add files via upload

---
+++ device.js
@@ -0,0 +1,19 @@
+function detectDevice() {
+  let w = window.innerWidth;
+  let body = document.body;
+
+  body.classList.remove("phone", "tablet", "desktop");
+
+  if (w <= 768) {
+    body.classList.add("phone");
+  } 
+  else if (w <= 1024) {
+    body.classList.add("tablet");
+  } 
+  else {
+    body.classList.add("desktop");
+  }
+}
+
+window.addEventListener("load", detectDevice);
+window.addEventListener("resize", detectDevice);
